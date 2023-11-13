37ed183cc80442b5d06c6e3c9facd8e801dcd9ac
cgathua <114153640+cgathua@users.noreply.github.com>
2023-11-13 11:20:29 -0500

    create: reviews section

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ new Chart(ctx, {
         }
     }
 });
+// end of chart js
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // change slidesPerView above 600px
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        }
+    }
+});
